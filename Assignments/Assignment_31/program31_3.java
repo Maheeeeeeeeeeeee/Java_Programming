12a43ca32eab52dcb64c5b07b4f9ac3a3bd9a32c
Maheeeeeeeeeeeee <maheshboke2003@gmail.com>
2026-02-25 18:59:58 +0530

    Add files via upload

---
+++ Assignments/Assignment_31/program31_3.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Method Name:    Pattern
+//      Description:    Displays a mixed symbol pattern using '#', '$' and '*' based on row–column relations
+//      Input:          int, int
+//      Output:         void
+//      Author:         Mahesh Dhanyakumar Boke
+//      Date:           03-12-2025
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class Display
+{
+    public void Pattern(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(i == j)
+                {
+                    System.out.print("$\t");
+                }
+                else if(j < i)
+                {
+                    System.out.print("#\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+class program31_3
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0, iValue2 = 0;
+
+        Scanner eobj = new Scanner(System.in);
+        System.out.print("Enter number of rows and columns : ");
+        iValue1 = eobj.nextInt();
+        iValue2 = eobj.nextInt();
+    
+        Display dobj = new Display();
+        dobj.Pattern(iValue1, iValue2);
+    
+    }
+}
---
+++ Assignments/Assignment_31/program31_4.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Method Name:    Pattern
+//      Description:    Displays a hollow rectangle with a diagonal using stars based on given dimensions
+//      Input:          int, int
+//      Output:         void
+//      Author:         Mahesh Dhanyakumar Boke
+//      Date:           03-12-2025
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class Display
+{
+    public void Pattern(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(i == j)
+                {
+                    System.out.print("*\t");
+                }
+                else if(i == 1 || i == iRow || j == 1 || j == iCol)
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print("\t");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+class program31_4
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0, iValue2 = 0;
+
+        Scanner eobj = new Scanner(System.in);
+        System.out.print("Enter number of rows and columns : ");
+        iValue1 = eobj.nextInt();
+        iValue2 = eobj.nextInt();
+    
+        Display dobj = new Display();
+        dobj.Pattern(iValue1, iValue2);
+    
+    }
+}
